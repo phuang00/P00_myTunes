d39865d95c06815258ff4561560bdf7e5403363e
phuang00 <phuang00@stuy.edu>
2019-10-29 22:41:08 -0400

    added testcases

--- main.c
+++ main.c
@@ -4,6 +4,11 @@ int main() {
   struct song_node *songs = NULL;
   songs = insert_front(songs, "street spirit (fade out)", "radiohead");
   songs = insert_front(songs, "paranoid android", "radiohead");
+  songs = insert_front(songs, "time", "pink floyd");
+  songs = insert_front(songs, "yellow ledbetter", "pearl jam");
+  songs = insert_front(songs, "even flow", "pearl jam");
+  songs = insert_front(songs, "alive", "pearl jam");
+  songs = insert_front(songs, "thunderstruck", "ac/dc");
 
   printf("LINKED LIST TESTS\n====================================\n\n");
   printf("Testing print_list:\n");
