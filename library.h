c52efe350ab162d607553bd875165b1378d398ce
Jackie Lin <jlin00@stuy.edu>
2019-10-30 14:26:42 -0400

    fixed function headers

--- library.h
+++ library.h
@@ -1,19 +1,19 @@
 #include "linked_list.h"
 
-struct song_node * add_song(struct song_node *input[27], struct song_node);
+struct song_node * add_song(struct song_node *input[27], char n[100], char a[100]);
 
-struct song_node * find_song(struct song_node *input[27], struct song_node);
+struct song_node * find_song(struct song_node *input[27], char n[100], char a[100]);
 
-struct song_node * find_artist(struct song_node *input[27], struct song_node);
+struct song_node * find_artist(struct song_node *input[27], char a[100]);
 
-struct song_node * print_artists(struct song_node *input[27], struct song_node);
+void print_letter(struct song_node *input[27], char ltr);
 
-struct song_node * print_songs(struct song_node *input[27], struct song_node);
+void * print_songs(struct song_node *input[27], char a[100]);
 
 void print_library(struct song_node *input[27]);
 
-struct song_node * shuffle(struct song_node *input[27], struct song_node);
+void shuffle(struct song_node *input[27]);
 
-struct song_node * delete_song(struct song_node *input[27], struct song_node);
+struct song_node * delete_song(struct song_node *input[27], char n[100], char a[100]);
 
-struct song_node * clear_library(struct song_node *input[27], struct song_node);
+struct song_node * clear_library(struct song_node *input[27]);
