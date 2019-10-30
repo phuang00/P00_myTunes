2c02e46d2ab865e3c22264b2ceba77d8e6ebb844
Jackie Lin <jlin00@stuy.edu>
2019-10-30 14:28:04 -0400

    added dummy fxns to library.c

--- library.c
+++ library.c
@@ -1,5 +1,25 @@
 #include "library.h"
 
+struct song_node * add_song(struct song_node *input[27], char n[100], char a[100]){
+  return NULL;
+}
+
+struct song_node * find_song(struct song_node *input[27], char n[100], char a[100]){
+  return NULL;
+}
+
+struct song_node * find_artist(struct song_node *input[27], char a[100]){
+  return NULL;
+}
+
+void print_letter(struct song_node *input[27], char ltr){
+
+}
+
+void * print_songs(struct song_node *input[27], char a[100]){
+
+}
+
 void print_library(struct song_node *input[27]){
   int i;
   for (i = 0; i < 27; i++){
@@ -9,4 +29,16 @@ void print_library(struct song_node *input[27]){
     }
   }
   printf("\n");
+}
+
+void shuffle(struct song_node *input[27]){
+
+}
+
+struct song_node * delete_song(struct song_node *input[27], char n[100], char a[100]){
+  return NULL;
+}
+
+struct song_node * clear_library(struct song_node *input[27]){
+  return NULL;
 }
